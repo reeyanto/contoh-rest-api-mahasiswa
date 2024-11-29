6121b6e3bd536ee873c3ffe6f5e294f2fb6029ef
Riyanto <riyanto.droider@gmail.com>
2024-11-29 17:17:23 +0700

    Update layout tambah dan edit data

--- resources/views/admin/mahasiswa/create.blade.php
+++ resources/views/admin/mahasiswa/create.blade.php
@@ -19,40 +19,53 @@
                         <form action="{{ route('mahasiswa.store') }}" method="post">
                             @csrf
                             
-                            <div class="mb-3">
-                                <label for="nim">Nomor Induk Mahasiswa <span class="text-danger">*</span></label>
-                                <input type="text" name="nim" value="{{ old('nim') }}" autocomplete="off" class="form-control @error('nim') is-invalid @enderror">
-                                @error('nim')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                            <div class="row">
+                                <div class="col-lg-6 col-sm-12">
+                                    <div class="mb-3">
+                                        <label for="nim">Nomor Induk Mahasiswa <span class="text-danger">*</span></label>
+                                        <input type="text" name="nim" value="{{ old('nim') }}" autocomplete="off" class="form-control @error('nim') is-invalid @enderror" placeholder="Nomor Induk Mahasiswa">
+                                        @error('nim')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-sm-12">
+                                    <div class="mb-3">
+                                        <label for="nama">Nama Mahasiswa <span class="text-danger">*</span></label>
+                                        <input type="text" name="nama" value="{{ old('nama') }}" autocomplete="off" class="form-control @error('nama') is-invalid @enderror" placeholder="Nama Mahasiswa">
+                                        @error('nama')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="mb-3">
-                                <label for="nama">Nama Mahasiswa <span class="text-danger">*</span></label>
-                                <input type="text" name="nama" value="{{ old('nama') }}" autocomplete="off" class="form-control @error('nama') is-invalid @enderror">
-                                @error('nama')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="mb-3">
+                                        <label for="alamat">Alamat <span class="text-danger">*</span></label>
+                                        <textarea name="alamat" id="alamat" rows="3" autocomplete="off" class="form-control" style="resize: none" placeholder="Alamat">{{ old('alamat') }}</textarea>
+                                        @error('alamat')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="mb-3">
-                                <label for="alamat">Alamat <span class="text-danger">*</span></label>
-                                <textarea name="alamat" id="alamat" rows="3" autocomplete="off" class="form-control" style="resize: none">{{ old('alamat') }}</textarea>
-                                @error('alamat')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="prodi">Program Studi <span class="text-danger">*</span></label>
-                                <select name="prodi" id="prodi" class="form-control">
-                                    <option value="">Pilih Program Studi</option>
-                                    <option value="Manajemen Informatika" {{ old('prodi') == 'Manajemen Informatika' ? 'selected' : '' }}>Manajemen Informatika</option>
-                                    <option value="Teknik Listrik" {{ old('prodi') == 'Teknik Listrik' ? 'selected' : '' }}>Teknik Listrik</option>
-                                </select>
-                                @error('prodi')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                            <div class="row">
+                                <div class="col-lg-6 col-sm-12">
+                                    <div class="mb-3">
+                                        <label for="prodi">Program Studi <span class="text-danger">*</span></label>
+                                        <select name="prodi" id="prodi" class="form-control">
+                                            <option value="">Pilih Program Studi</option>
+                                            <option value="Manajemen Informatika" {{ old('prodi') == 'Manajemen Informatika' ? 'selected' : '' }}>Manajemen Informatika</option>
+                                            <option value="Teknik Listrik" {{ old('prodi') == 'Teknik Listrik' ? 'selected' : '' }}>Teknik Listrik</option>
+                                        </select>
+                                        @error('prodi')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
                             </div>
 
                             <div class="mb-3">
--- resources/views/admin/mahasiswa/edit.blade.php
+++ resources/views/admin/mahasiswa/edit.blade.php
@@ -20,40 +20,54 @@
                             @csrf
                             @method('PUT')
 
-                            <div class="mb-3">
-                                <label for="nim">Nomor Induk Mahasiswa <span class="text-danger">*</span></label>
-                                <input type="text" name="nim" value="{{ old('nim', $mahasiswa->nim) }}" autocomplete="off" readonly class="form-control bg-light @error('nim') is-invalid @enderror">
-                                @error('nim')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                            <div class="row">
+                                <div class="col-lg-6 col-sm-12">
+                                    <div class="mb-3">
+                                        <label for="nim">Nomor Induk Mahasiswa <span class="text-danger">*</span></label>
+                                        <input type="text" name="nim" value="{{ old('nim', $mahasiswa->nim) }}" autocomplete="off" readonly class="form-control bg-light @error('nim') is-invalid @enderror" placeholder="Nomor Induk Mahasiswa">
+                                        @error('nim')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-sm-12">
+                                    <div class="mb-3">
+                                        <label for="nama">Nama Mahasiswa <span class="text-danger">*</span></label>
+                                        <input type="text" name="nama" value="{{ old('nama', $mahasiswa->nama) }}" autocomplete="off" class="form-control @error('nama') is-invalid @enderror" placeholder="Nama Mahasiswa">
+                                        @error('nama')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="mb-3">
-                                <label for="nama">Nama Mahasiswa <span class="text-danger">*</span></label>
-                                <input type="text" name="nama" value="{{ old('nama', $mahasiswa->nama) }}" autocomplete="off" class="form-control @error('nama') is-invalid @enderror">
-                                @error('nama')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
+
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="mb-3">
+                                        <label for="alamat">Alamat <span class="text-danger">*</span></label>
+                                        <textarea name="alamat" id="alamat" rows="3" autocomplete="off" class="form-control" style="resize: none" placeholder="Alamat">{{ old('alamat', $mahasiswa->alamat) }}</textarea>
+                                        @error('alamat')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="mb-3">
-                                <label for="alamat">Alamat <span class="text-danger">*</span></label>
-                                <textarea name="alamat" id="alamat" rows="3" autocomplete="off" class="form-control" style="resize: none">{{ old('alamat', $mahasiswa->alamat) }}</textarea>
-                                @error('alamat')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="prodi">Program Studi <span class="text-danger">*</span></label>
-                                <select name="prodi" id="prodi" class="form-control">
-                                    <option value="">Pilih Program Studi</option>
-                                    <option value="Manajemen Informatika" {{ old('prodi', $mahasiswa->prodi) == 'Manajemen Informatika' ? 'selected' : '' }}>Manajemen Informatika</option>
-                                    <option value="Teknik Listrik" {{ old('prodi', $mahasiswa->prodi) == 'Teknik Listrik' ? 'selected' : '' }}>Teknik Listrik</option>
-                                </select>
-                                @error('prodi')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                            <div class="row">
+                                <div class="col-lg-6 col-sm-12">
+                                    <div class="mb-3">
+                                        <label for="prodi">Program Studi <span class="text-danger">*</span></label>
+                                        <select name="prodi" id="prodi" class="form-control">
+                                            <option value="">Pilih Program Studi</option>
+                                            <option value="Manajemen Informatika" {{ old('prodi', $mahasiswa->prodi) == 'Manajemen Informatika' ? 'selected' : '' }}>Manajemen Informatika</option>
+                                            <option value="Teknik Listrik" {{ old('prodi', $mahasiswa->prodi) == 'Teknik Listrik' ? 'selected' : '' }}>Teknik Listrik</option>
+                                        </select>
+                                        @error('prodi')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
                             </div>
 
                             <div class="mb-3">
